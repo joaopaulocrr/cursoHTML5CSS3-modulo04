8e9f5783068e264bf6c5f8b371962ab14fc7cda8
João Paulo <paulocorrentinense@gmail.com>
2023-02-25 20:44:13 -0300

    projeto-finalizado

--- media queries/mq005-menu/script.js
+++ media queries/mq005-menu/script.js
@@ -1,8 +1,16 @@
 function clicar() {
   var clique = document.getElementById("display").style.display;
-  
-  if(clique == "none")
-   document.getElementById("display").style.display = 'block';
+
+  if (clique == "none")
+    document.getElementById("display").style.display = 'block';
   else
-   document.getElementById("display").style.display = 'none';
+    document.getElementById("display").style.display = 'none';
+};
+
+function mudouTamanho() {
+  if (window.innerWidth >= 700) {
+    document.getElementById("display").style.display = 'block'
+  } else {
+    document.getElementById("display").style.display = 'none'
+  }
 };
